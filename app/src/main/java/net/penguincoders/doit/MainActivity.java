5b5e0e9cdd0deb9b35464e9188bd253eeb2a0c87
maram67 <150845494+maram67@users.noreply.github.com>
2023-12-04 00:26:40 +0300

    Add files via upload

---
+++ app/src/main/java/net/penguincoders/doit/MainActivity.java
@@ -0,0 +1,90 @@
+package net.penguincoders.doit;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.ItemTouchHelper;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.DialogInterface;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import net.penguincoders.doit.Adapters.ToDoAdapter;
+import net.penguincoders.doit.Model.ToDoModel;
+import net.penguincoders.doit.Utils.DatabaseHandler;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+import java.util.Objects;
+
+public class MainActivity extends AppCompatActivity implements DialogCloseListener {
+
+    private DatabaseHandler db;
+    private RecyclerView tasksRecyclerView;
+    private ToDoAdapter tasksAdapter;
+    private FloatingActionButton fab;
+    private List<ToDoModel> taskList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        hideActionBar();
+        initializeDatabase();
+        setupRecyclerView();
+        setupItemTouchHelper();
+        setupFab();
+    }
+
+    private void hideActionBar() {
+        Objects.requireNonNull(getSupportActionBar()).hide();
+    }
+
+    private void initializeDatabase() {
+        db = new DatabaseHandler(this);
+        db.openDatabase();
+        taskList = db.getAllTasks();
+        Collections.reverse(taskList);
+    }
+
+    private void setupRecyclerView() {
+        tasksRecyclerView = findViewById(R.id.tasksRecyclerView);
+        tasksRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        tasksAdapter = new ToDoAdapter(db, MainActivity.this);
+        tasksAdapter.setTasks(taskList);
+        tasksRecyclerView.setAdapter(tasksAdapter);
+    }
+
+    private void setupItemTouchHelper() {
+        ItemTouchHelper itemTouchHelper = new ItemTouchHelper(new RecyclerItemTouchHelper(tasksAdapter));
+        itemTouchHelper.attachToRecyclerView(tasksRecyclerView);
+    }
+
+    private void setupFab() {
+        fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AddNewTask.newInstance().show(getSupportFragmentManager(), AddNewTask.TAG);
+            }
+        });
+    }
+
+    @Override
+    public void handleDialogClose(DialogInterface dialog) {
+        taskList = db.getAllTasks();
+        Collections.reverse(taskList);
+        tasksAdapter.setTasks(taskList);
+        tasksAdapter.notifyDataSetChanged();
+    }
+}
